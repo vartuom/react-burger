6be384d652fd59d2d0620afe2ca364dc57e04422
vartuom <93790667+vartuom@users.noreply.github.com>
2022-05-30 16:57:35 +0800

    30-05-from-work

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Находим DOM-элемент для отрисовки в нем модальных окон
+const modalsContainer = document.querySelector('#modals');
+
+const Modal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Modal;
---
+++ src/components/modalOverlay/modalOverlay.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import modalOverlayStyles from "./modalOverlay.module.css"
+
+const ModalOverlay = () => {
+    return (
+        <div className={modalOverlayStyles.overlay}>
+        </div>
+    );
+};
+
+export default ModalOverlay;
